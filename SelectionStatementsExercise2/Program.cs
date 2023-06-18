61b9ca6906ad95ec8a75a42724b38514cd67de21
Joshua Swan <thejoshuaswan@gmail.com>
2023-06-18 02:08:00 -0500

    finished exercise 2

--- SelectionStatementsExercise2/Program.cs
+++ SelectionStatementsExercise2/Program.cs
@@ -4,7 +4,30 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello! What's your favorite subject in school?");
+            var answer = Console.ReadLine();
+
+            switch (answer.ToLower())
+            {
+                case "math":
+                    Console.WriteLine("I think I can tell");
+                    break;
+                case "computer science":
+                    Console.WriteLine("Bit on the nose, don't you think?");
+                    break;
+                case "english":
+                    Console.WriteLine("Oo, someone's feeling pretentious");
+                    break;
+                case "gym":
+                    Console.WriteLine("Oh I can definitely tell");
+                    break;
+                case "geography":
+                    Console.WriteLine("Name every country right now");
+                    break;
+                default:
+                    Console.WriteLine("yeah, I didn't write a response for that, pick again bozo");
+                    break;
+            }
         }
     }
 }
